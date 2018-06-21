4fd012960ef5502ea90e1b4476f9fd91189b0584
Hans Wannop <hanswannop@me.com>
2018-06-21 17:29:43 +0200

    Use buffer and hide cursor when redrawing screen to eliminate possible flicker.

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -93,24 +94,53 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** append buffer ***/
+struct abuf {
+    char *b;
+    int len;
+};
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}\
+
+#define ABUF_INIT {NULL, 0}
+
 /*** output ***/
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
+
         if (y < E.screenrows -1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT; // Create new buffer
+    
+    abAppend(&ab, "\x1b[?25l", 6); //Hide cursor
+    abAppend(&ab, "\x1b[2J", 4); // Clear screen
+    abAppend(&ab, "\x1b[H", 3); // Zero cursor
 
-    editorDrawRows();
+    editorDrawRows(&ab); // Draw tildes
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 3); // Zero cursor
+    abAppend(&ab, "\x1b[?25h", 6); //Show cursor
+
+    write(STDOUT_FILENO, ab.b, ab.len); // Write out buffer
+    abFree(&ab); // Free buffer
 }
 
 /*** input ***/
